06e01a7c4f2d5ce29ca5eaa9c0c915490e7ddf58
MatsiboraK <kmacibora50@gmail.com>
2023-03-02 22:35:51 +0700

    edit Java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int ticket_price = 13676;
+        int rub_per_mile = 20;
+        int bonus_miles = ticket_price / rub_per_mile;
+
+System.out.println(bonus_miles);
     }
 }
